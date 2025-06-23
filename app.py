df2a18bece44c3477fdc665c4038ee90470aa6ec
DacNV95 <54349466+DacNV95@users.noreply.github.com>
2025-06-23 14:18:01 +0700

    a

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ R2_ACCESS_KEY = os.getenv("R2_ACCESS_KEY")
 R2_SECRET_KEY = os.getenv("R2_SECRET_KEY")
 R2_ACCOUNT_ID = os.getenv("R2_ACCOUNT_ID")
 R2_BUCKET = os.getenv("R2_BUCKET")
-R2_ENDPOINT = f'https://{R2_ACCOUNT_ID}.r2.cloudflarestorage.com'
+R2_ENDPOINT = f"https://{R2_BUCKET}.r2.dev/{filename}"
 
 s3 = boto3.client('s3',
     endpoint_url=R2_ENDPOINT,
